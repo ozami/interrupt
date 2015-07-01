906520771d8af9e2b303f5e73451b53ae9209e81
unknown <ozawamasayuki@1078-AT992E.CETUS-DOMAIN.cetus.local>
2015-07-01 16:55:11 +0900

    3秒後に表示

--- index.js
+++ index.js
@@ -5,15 +5,17 @@
     if (Notify.permissionLevel == "default") {
         Notify.requestPermission();
     }
-    new Notify("割り込みＯＫ？", {
-        body: "割り込み歓迎の場合はクリックしてください。",
-        icon: "alert.ico",
-        timeout: 5,
-        notifyClose: function() {
-            alert("closed");
-        },
-        notifyClick: function() {
-            alert("clicked");
-        }
-    }).show();
+    setTimeout(function() {
+        new Notify("割り込みＯＫ？", {
+            body: "割り込み歓迎の場合はクリックしてください。",
+            icon: "alert.ico",
+            timeout: 5,
+            notifyClose: function() {
+                alert("closed");
+            },
+            notifyClick: function() {
+                alert("clicked");
+            }
+        }).show();
+    }, 3000);
 })();
