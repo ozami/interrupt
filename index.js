25bc14cbc366cb21737bf80a9726e2962800c6e6
unknown <ozawamasayuki@1078-AT992E.CETUS-DOMAIN.cetus.local>
2015-07-06 16:19:42 +0900

    ko.js 使う 通知の許可状態でボタンの状態を制御

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
-(function() {
-    if (!Notify.isSupported) {
-        return;
-    }
-    if (Notify.permissionLevel == "default") {
-        Notify.requestPermission();
-    }
-    setTimeout(function() {
-        new Notify("割り込みＯＫ？", {
-            body: "割り込み歓迎の場合はクリックしてください。",
-            icon: "alert.ico",
-            timeout: 5,
-            notifyClose: function() {
-                alert("closed");
-            },
-            notifyClick: function() {
-                alert("clicked");
-            }
-        }).show();
-    }, 3000);
-})();
+var Interrupt = function() {
+    var self = this;
+    self.notify_granted = ko.observable(false);
+    
+    self.checkPermission = function() {
+        if (!Notify.isSupported) {
+            alert("お使いのブラウザーでは通知機能が使えません");
+            return;
+        }
+        if (Notify.permissionLevel == "granted") {
+            self.notify_granted(true);
+        }
+        else {
+            Notify.requestPermission(function() {
+                self.notify_granted(true);
+            });
+        }
+    };
+};
+
+var interrupt = new Interrupt();
+ko.applyBindings(interrupt);
+
+
+setTimeout(function() {return;
+    new Notify("割り込みＯＫ？", {
+        body: "割り込み歓迎の場合はクリックしてください。",
+        icon: "alert.ico",
+        timeout: 5,
+        notifyClose: function() {
+            alert("closed");
+        },
+        notifyClick: function() {
+            alert("clicked");
+        }
+    }).show();
+}, 3000);
+
