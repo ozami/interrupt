eb5bc7595049f8a235e8225589d6d67416c5ba1f
unknown <ozawamasayuki@1078-AT992E.CETUS-DOMAIN.cetus.local>
2015-07-08 11:28:38 +0900

    通知をまともにループさせる

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var Interrupt = function() {
     var self = this;
     self.notify_granted = ko.observable(false);
-    self.interrupt_timer = ko.observable(null);
-    self.next_notify = ko.observable(null);
+    self.interrupt_ok = ko.observable(false);
+    self.next_notify_time = ko.observable(null);
+    self.is_start_button_enabled = ko.computed(function() {
+        return self.notify_granted() && !self.interrupt_ok();
+    });
+    
     self.checkPermission = function() {
         if (!Notify.isSupported) {
             alert("お使いのブラウザーでは通知機能が使えません");
@@ -19,29 +23,37 @@ var Interrupt = function() {
     };
     
     self.startInterruptOk = function() {
-        if (self.interrupt_timer()) {
+        if (self.interrupt_ok()) {
             return;
         }
-        self.next_notify(new Date().addMinutes(5));
-        self.interrupt_timer(
-            setInterval(function() {
-                if (self.next_notify().isFuture()) {
-                    return;
-                }
-                new Notify("割り込みＯＫ？", {
-                    body: "割り込まれても大丈夫な場合はクリックしてください。",
+        self.interrupt_ok(true);
+        var interval = 3000;//5 * 60 * 1000;
+        var loop = function() {
+            self.next_notify_time(new Date().addMilliseconds(interval));
+            var next_notify = setTimeout(function() {
+                var notify = new Notify("割り込みＯＫ？", {
+                    body: "割り込みが入ってもよい場合はクリックしてください。",
                     icon: "alert.ico",
-                    timeout: 10,
-                    notifyClose: function() {
-                        //alert("closed");
-                    },
                     notifyClick: function() {
-                        self.next_notify(new Date().addMinutes(5));
+                        loop();
                     }
-                }).show();
-            }, 2000)
-        );
+                });
+                notify.show();
+                var timeout = setTimeout(function() {
+                    notify.close();
+                    self.stopInterruptOk();
+                }, 10 * 1000);
+            }, interval);
+        };
+        loop();
     };
+    
+    self.stopInterruptOk = function() {
+        if (!self.interrupt_ok()) {
+            return;
+        }
+        self.interrupt_ok(false);
+    }
 };
 
 var interrupt = new Interrupt();
